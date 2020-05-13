c8e86f465a594651818e92075d887b3fc12409bf
JunHeon-Ch <chjh12100@gmail.com>
2020-05-13 14:55:44 +0900

    feat: modify filter function in my closet
    modify filter function in my closet
    - From Filter Activity, send the filter conditions to My Closet Fragment.

--- MP_termProject/app/src/main/java/com/example/mp_termproject/mycloset/MyClosetFragment.java
+++ MP_termProject/app/src/main/java/com/example/mp_termproject/mycloset/MyClosetFragment.java
@@ -8,6 +8,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,8 +19,12 @@ import com.example.mp_termproject.R;
 import com.example.mp_termproject.mycloset.add.MyClosetAddActivity;
 import com.example.mp_termproject.mycloset.filter.MyClosetFilterActivity;
 
+import java.util.ArrayList;
+
 
 public class MyClosetFragment extends Fragment {
+
+    static final int REQUEST_FILTER = 1;
 
     @Nullable
     @Override
@@ -32,7 +37,12 @@ public class MyClosetFragment extends Fragment {
                 container, false);
         setHasOptionsMenu(true);
 
+
+
+//        상운 구현부
 //        데이터베이스에서 내 옷장에 있는 옷 읽어와서 뿌려주는거 구현
+
+
 
 
         return rootView;
@@ -71,9 +81,45 @@ public class MyClosetFragment extends Fragment {
 //                필터 선택 후 My Closet 화면에 조건에 맞는 아이템을 보여줌
 
                 intent = new Intent(getContext(), MyClosetFilterActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, REQUEST_FILTER);
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_FILTER){
+            if(resultCode == -1){
+                Bundle bundle = data.getExtras();
+
+                ArrayList<String> categoryItemList = bundle.getStringArrayList("category");
+                ArrayList<String> colorItemList = bundle.getStringArrayList("color");
+                ArrayList<String> seasonItemList = bundle.getStringArrayList("season");
+                String sharedItem = bundle.getString("share");
+
+
+
+//                               상운 구현부
+//                categorySelectedList, colorSelectedList, seasonSelectedList, shareSelected에
+//                저장된 데이터들이 필터 기준임.
+//                예를들어, categorySelectedList에  상의 Top, 아우터 Outer 이렇게 저장되있으면
+//                "상의, 아우터만 데이터베이스에서 가져와라" 이 뜻
+//                만약 리스트가 null인 경우, 필터 기준없이 다 가져오면 됨.
+//                예를 들어, 카테고리 -> 상의 / 컬러 -> null / 시즌 -> 봄 / 공유 -> 비공유 이면
+//                "카테고리가 상의고, 시즌은 봄이고, 공유는 비공유이고, 컬러는 모든 컬러를 가져와라"
+
+
+
+                Toast.makeText(getContext(),
+                        categoryItemList.toString() + "\n"
+                                + colorItemList.toString() + "\n"
+                                + seasonItemList.toString() + "\n"
+                                + sharedItem + "\n",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
